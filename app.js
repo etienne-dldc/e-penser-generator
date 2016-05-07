8a540545a00ddd73d88949b0c1f4f4cb54d889aa
Etienne Dldc <etienne.dldc@outlook.fr>
2016-05-08 00:28:10 +0200

    Minor imporements

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 $(function() {
   'use strict';
 
+  var lastObj = null;
+  var lastScience = null;
+
   var objectsList = [
     { name:'porte', img:'porte.png' },
     { name:'tabouret', img:'tabouret.png' },
     { name:'chaise', img:'chaise.png' },
-    { name:'boite de concerve', img:'boite.png' }
+    { name:'boite de concerve', img:'boite.png' },
+    { name:'clé à molette', img:'cle.png' },
+    { name:'enjoliveur', img:'enjoliveur.png' }
   ];
 
   var scienceList = [
     'intrication quantique',
     'physique quantique',
     'moment cinétique',
-    'ambiguïté cellulaire'
+    'ambiguïté cellulaire',
+    'téléportation quantique',
+    'paradoxe EPR'
   ];
 
   var $btn = $('.generate-btn');
@@ -39,15 +46,21 @@ $(function() {
   } // end randomInRange
 
   function generate() {
-    var obj = randomInList(objectsList);
-    var science = randomInList(scienceList);
+    do {
+      var obj = randomInList(objectsList);
+    } while (obj == lastObj);
+    lastObj = obj;
+    do {
+      var science = randomInList(scienceList);
+    } while (science == lastScience);
+    lastScience = science
     var numEpisode = randomInRange(37, 200);
-    var title = obj.name + ' et ' + science + ' — ' + numEpisode + ' - e-penser';
+    var title = lastObj.name + ' et ' + lastScience + ' — ' + numEpisode + ' - e-penser';
     title = title.charAt(0).toUpperCase() + title.slice(1);
 
     // Apply
     $title.html(title);
-    $img.attr('src', 'images/' + obj.img);
+    $img.attr('src', 'images/' + lastObj.img);
   } // end generateName
 
 
